eec85b98c67f041fe7f59cce6d1770df34739a62
reshalfahsi <resha.alfahsi@gmail.com>
2020-07-27 14:40:18 +0700

    update cv

--- js/meme.js
+++ js/meme.js
@@ -1,7 +1,7 @@
 function meme(){
 
 var myimages=new Array()
-myimages=["https://i.ibb.co/PmBFVg3/aiengineermeme.png", "https://i.ibb.co/VNQVR9X/jian-yang-hot-dog-gif.gif", "https://media.giphy.com/media/xT9DPp7lYtKlM0QzII/giphy.gif", "https://miro.medium.com/max/1564/1*hBccbACZZkXyRl_YI1abFQ.jpeg", "https://images.ctfassets.net/be04ylp8y0qc/1AYDDqzqoLNU7GyvMOLlTL/146ab0e8b96a25c1b3538a2929dae646/meme_6cce0f3bba63820b84f52ce05a822975_800.png?fm=jpg"
+myimages=["https://i.ibb.co/PmBFVg3/aiengineermeme.png", "https://i.ibb.co/VNQVR9X/jian-yang-hot-dog-gif.gif", "https://media.giphy.com/media/xT9DPp7lYtKlM0QzII/giphy.gif", "https://miro.medium.com/max/1564/1*hBccbACZZkXyRl_YI1abFQ.jpeg", "https://images.ctfassets.net/be04ylp8y0qc/1AYDDqzqoLNU7GyvMOLlTL/146ab0e8b96a25c1b3538a2929dae646/meme_6cce0f3bba63820b84f52ce05a822975_800.png?fm=jpg", "https://www.mkanalysis.com/static/img/Data%20Science%20Tom%20and%20Jerry.jpg"
 ]
 
 var ry=Math.floor(Math.random()*myimages.length)
